17f29396e559629c1e7b9711d01b358e2b8816ae
Jokir <doner24072004@gmail.com>
2024-04-15 11:17:58 +0300

    Input Decorator

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+
+  @Input({
+    required:true
+  })
+  course:Course;
+  
+  onCourseViewed(){
+    alert("You clicked!")
+  }
+
+}
