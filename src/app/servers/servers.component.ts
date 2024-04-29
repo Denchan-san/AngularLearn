5cdf63a2543623091277ffdf89e132808e8e9938
Jokir <doner24072004@gmail.com>
2024-04-29 23:39:40 +0300

    Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer: boolean = false;
 
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000)
+  }
 }
