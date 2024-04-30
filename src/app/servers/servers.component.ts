e230e2ae5305e7cd286e8781b051cb569f28144b
Jokir <doner24072004@gmail.com>
2024-04-30 23:26:51 +0300

    two-way-databiding assigment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,17 +8,37 @@ import { Component } from '@angular/core';
   // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent {
+
+  //username: string = null;
+  username = '';
+  usernameIsEmpty() {
+    if(this.username == null || this.username.length == 0){
+      return true;
+    }
+    else false;
+  }
+
+  emptyUsername(){
+    this.username = null;
+  }
+
+  serverName = '';
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
-
+  
   constructor(){
     setTimeout(()=>{
       this.allowNewServer = true;
     },2000)
   }
-
+  
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
